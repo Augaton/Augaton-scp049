d39fcf8c61162f095c7309b330d4a0a77d15e92b
Augaton <Mulleraurelien@hotmail.fr>
2025-12-19 11:07:08 +0100

    [Fix] Little optimisation and fallback to server side

--- lua/guthscp/modules/augscp049/server.lua
+++ lua/guthscp/modules/augscp049/server.lua
@@ -1,18 +1,25 @@
 local augscp049 = guthscp.modules.augscp049
 local config049 = guthscp.configs.augscp049
 
+local disableJump = config049.disable_jump
+local isImmortal = config049.scp049_immortal
+
 hook.Add("SetupMove", "augscp049:no_move", function(ply, mv, cmd)
+    if not disableJump then return end
     if not augscp049.is_scp_049(ply) then return end
     if ply:GetMoveType() == MOVETYPE_NOCLIP then return end
 
-    if guthscp.configs.augscp049.disable_jump then
+    if mv:KeyPressed(IN_JUMP) then
         mv:SetButtons(bit.band(mv:GetButtons(), bit.bnot(IN_JUMP)))
     end
 end)
 
--- Hook pour gérer les dégâts des SCP-049
-hook.Add("PlayerShouldTakeDamage", "augscp049:no_damage", function(ply)
-    if config049.scp049_immortal and augscp049.is_scp_049(ply) then
-        return false
+hook.Add("EntityTakeDamage", "augscp049:prevent_damage", function(target, dmginfo)
+    if not isImmortal then return end
+    
+    if target:IsPlayer() and augscp049.is_scp_049(target) then
+        dmginfo:SetDamage(0)
+        dmginfo:ScaleDamage(0)
+        return true
     end
 end)
